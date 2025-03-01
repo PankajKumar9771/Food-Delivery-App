1ecf84cd002ca92879b78588adba978b888448f7
Pankaj Kumar <pankajkumar9771774088@gmail.com>
2025-03-01 15:44:23 +0530

    updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Add from "./pages/Add/Add";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const url = "http://localhost:4000";
+const url = "https://food-delivery-app-backend-vnzz.onrender.com";
 
 const App = () => {
   return (
